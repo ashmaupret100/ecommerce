0ab9d32c0eba2a526b8d373f137a2f8e543a692d
Ashma Upreti <ashma.upreti99@gmail.com>
2023-07-27 00:20:59 -0400

    initial setup and installment

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,42 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Homepage from "./pages/home-page/home-page.tsx";
+import ProductDetailPage from "./pages/product-detail-page/product-detail-page.tsx";
+import ProductListPage from "./pages/product-list-page/product-list-page.tsx";
+import Register from "./pages/register/register.tsx";
+import Login from "./pages/login/login.tsx";
+import ErrorPage from "./pages/error-page/error-page.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Homepage />,
+  },
+  {
+    path: "/each-item",
+    element: <ProductDetailPage />,
+  },
+  {
+    path: "/products",
+    element: <ProductListPage />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/signin",
+    element: <Login />,
+  },
+  {
+    path: "/error",
+    element: <ErrorPage />,
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/pages/home-page/home-page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Homepage() {
+  return <div className="bg-red-100">Homepage</div>;
+}
+
+export default Homepage;
